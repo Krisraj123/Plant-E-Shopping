7aac1fafc0497ef7c842e451691256565088951c
Krisraj123 <krisraj70@gmail.com>
2024-11-19 16:47:25 +0530

    added quantity variable

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,11 +1,25 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+
+import { useDispatch, useSelector } from 'react-redux';
 function ProductList() {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
 
     const [addedToCart,setAddedToCart] =  useState({});
+
+    const [totalQuantity, setTotalQuantity] = useState(0);
+    const dispatch = useDispatch();
+
+    const cartItems = useSelector(state => state.cart.items);
+
+    useEffect(() => {
+        const quantity = cartItems.reduce((total, item) => total + item.quantity, 0);
+        console.log(quantity);
+        setTotalQuantity(quantity);
+    }, [cartItems]);
 
     const plantsArray = [
         {
@@ -244,8 +258,7 @@ const handlePlantsClick = (e) => {
     setShowCart(false); // Hide the cart when navigating to About Us
 };
 
-   const handleContinueShopping = (e) => {
-    e.preventDefault();
+   const handleContinueShopping = () => {
     setShowCart(false);
   };
 
